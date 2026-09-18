fb8ed89af3f603da9d949823de90274f309b7889
Veronika Ivhed <veronika.ivhed@mavatar.com>
2026-09-18 14:49:20 +0200

    Remove unreachable empty content rendering

--- frontend/src/todos/components/TodoList.jsx
+++ frontend/src/todos/components/TodoList.jsx
@@ -1,17 +1,9 @@
-import { Alert, Card, CardContent, Typography } from '@mui/material'
+import { Alert } from '@mui/material'
 import { useCallback } from 'react'
 import { useFetchData } from '../../hooks/useFetchData'
 import { fetchData } from '../../utils/fetchData'
 import { LoadingIndicator } from './LoadingIndicator'
 import { TodoListForm } from './TodoListForm'
-
-const NoTodoListFoundMessage = ({ style }) => (
-  <Card style={style}>
-    <CardContent>
-      <Typography variant='body1'>No todo list found.</Typography>
-    </CardContent>
-  </Card>
-)
 
 export const TodoList = ({ id, onUpdate }) => {
   const { data: todoList, isLoading, error } = useFetchData(`/todo-lists/${id}`)
@@ -40,8 +32,6 @@ export const TodoList = ({ id, onUpdate }) => {
         Something went wrong while fetching the todo list.
       </Alert>
     )
-
-  if (!todoList) return <NoTodoListFoundMessage style={{ margin: '0 1rem' }} />
 
   return <TodoListForm todoList={todoList} saveTodoList={handleSaveTodoList} />
 }
